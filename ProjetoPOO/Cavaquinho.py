195fe677104487fd4b3fc32325a6bd5125db74a2
joseroldao27062002 <roldao27602@gmail.com>
2022-07-14 00:15:23 -0300

    update

--- ProjetoPOO/Cavaquinho.py
+++
@@ -1,9 +0,0 @@
-from Violao import Violao
-class Cavaquinho(Violao):
-    def __init__(self, nome, fabricante, tipo, cor, numCordas, tipoAfinacao):
-        super().__init__(nome, fabricante, tipo, cor, numCordas)
-        self.tipoAfinacao = tipoAfinacao
-
-    def afinar(self):
-        return 'Afinando cavaquinho' 
-
--- projetosPython/primeiroCodigo.py
+++
@@ -1,2 +0,0 @@
-print('Olá VsCode esse é meu primeiro codigo em python no VSCode')
-print('Estudarei Django para ser um desenvolvedor web')
